5f0c15bb6dc821b32b981daec7a9691b2b869c9c
jcweston <54505249+jcweston@users.noreply.github.com>
2020-09-26 13:42:52 +0100

    attempted to add gallery

---
+++ src/Images/images.js
@@ -0,0 +1,12 @@
+const source = '../Gallery/1 Chebsey/'
+
+const images = [
+    {
+        id: 0,
+        src: `${source}AP70 Chebsey in the 1930s Newspaper cutting`,
+        name: 'cutting',
+        alt: 'AP70 Chebsey in the 1930s Newspaper cutting'
+    }
+]
+
+export default images
--- src/Pages/SubGallery.jsx
+++ src/Pages/SubGallery.jsx
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
+import SmartGallery from 'react-smart-gallery'
+import images from '../Images/images'
+
 
 class SubGallery extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { 
+            images: [
+                images[0].src
+            ]
+         }
     }
-    render() { 
+    render() {
         return ( 
             <div className='page'>
                 <p>{this.props.name}</p>
+                <SmartGallery images={this.state.images} />
             </div>
             
          );
     }
 }
+
  
 export default SubGallery;
